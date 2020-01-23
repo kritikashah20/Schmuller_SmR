7512b53821b435b815267dc5ea8e757706213a8e
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-01-23 17:09:07 +0100

    First chapter read.

---
+++ ch020.R
@@ -0,0 +1,103 @@
+# notes for  chapter 2
+
+# get / set working directory
+getwd()
+
+setwd("/Users/schmis12/wrk/studio/Schmuller_SmR/data")
+getwd()
+
+setwd("/Users/schmis12/wrk/studio/Schmuller_SmR")
+getwd()
+
+
+# create a vector
+x <- c(3, 4, 5)
+
+
+# list all objects
+ls()
+
+
+# sum up all vector elements
+sum(x)
+
+# arithmetic mean
+mean(x)
+
+# variance
+var(x)
+
+
+# NAs
+capacities <- c(390, 350, 390, 360, 420, NA, NA, 566, NA)
+mean(capacities)
+mean(capacities, na.rm = TRUE)
+
+# check for NAs
+is.na(capacities)
+
+# any NAs
+any(is.na(capacities))
+
+
+# user defined functions
+sumofsquares <- function(x, y, z) {
+  return(sum(c(x ^ 2, y ^ 2, z ^ 2)))
+}
+
+sumofsquares(3, 4, 5)
+
+
+## numeric vectors
+
+# integer
+y <- c(10, 20, 30)
+typeof(y)
+
+# double
+z <- c(10L, 20L, 30L)
+typeof(z)
+
+# sequences
+u <- 10:30
+u
+v <- seq(10, 30)
+v
+w <- seq(10, 30, 2)
+w
+
+# repeat
+tri <- c(6, 8, 2)
+rep_tri <- rep(tri, 4)
+rep_tri
+
+## matrix
+
+# create matrix from vector
+num_mat <- seq(5, 100, 5)
+dim(num_mat) <- c(5, 4)
+num_mat
+
+# or using matrix function
+num_mat <- matrix(seq(5, 100, 5), nrow = 5)
+num_mat
+
+# transpose matrix
+t(num_mat)
+
+## factor
+eye_color <- c(2, 2, 4, 1, 5, 5, 5, 6, 1, 3, 6, 3, 1, 4)
+f_eye_color <- factor(eye_color)
+levels(f_eye_color) <- c("amber", "blue", "brown", "gray", "green", "hazel")
+f_eye_color
+
+## list
+# vector of empathy scores
+empathy_score <- c(15, 21, 45, 32, 61, 74, 53, 92, 83, 22, 67, 55, 42, 44)
+
+# list
+eyes_and_empathy <- list(eyes_code = eye_color,
+                         eyes = f_eye_color,
+                         empathy = empathy_score)
+
+eyes_and_empathy
