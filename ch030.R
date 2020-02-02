67a4ecca4751d8d400467daa0e7b21c38ebfd568
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-02-02 17:58:25 +0100

    chapter 3 started.

--- ch030.R
+++ ch030.R
@@ -1,3 +1,51 @@
+library(tidyverse)
 library(MASS)
 
-Cars93
+
+
+## histogram
+
+# distribution of price
+# (bins of $5,000 between $0 and $65,000)
+ggplot(data = Cars93) + aes(Price) + geom_histogram(breaks = seq(0, 65, 5)) +
+  xlab("Price (x $1,000)") + ylab("Frequency")
+
+
+
+## barchart
+
+# distribution of type
+ggplot(data = Cars93) + aes(Cars93$Type) + geom_bar() +
+  xlab("Type") + ylab("Frequency")
+
+
+
+## scatter plot
+
+# base R
+plot(Cars93$Horsepower, Cars93$MPG.city)
+
+# ggplot2
+ggplot(data = Cars93) + aes(x = Cars93$Horsepower, y = Cars93$MPG.city) +
+  geom_point() +
+  xlab("Horsepower") + ylab("MPG City")
+
+# colored 3rd variable (type)
+ggplot(data = Cars93) +
+  aes(x = Cars93$Horsepower,
+      y = Cars93$MPG.city,
+      color = Type) +
+  geom_point() +
+  xlab("Horsepower") + ylab("MPG City")
+
+
+
+## box plots
+
+# horsepower ~ cylinders
+ggplot(data = Cars93) + aes(x = Cylinders, y = Horsepower) + geom_point()
+
+# as boxplot
+ggplot(data = Cars93) + aes(x = Cylinders, y = Horsepower) + geom_boxplot()
+
+
