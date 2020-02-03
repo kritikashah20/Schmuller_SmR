956f6237f2b977301b7911ac12727bed5dc379e0
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2020-02-03 17:56:54 +0100

    base R graphs.

--- ch030.R
+++ ch030.R
@@ -1,22 +1,56 @@
 library(tidyverse)
 library(MASS)
 
-
+### base R graphics
 
 ## histogram
 
 # distribution of price
+hist(Cars93$Price)
+
+# same with labels
+hist(Cars93$Price,
+     xlab = "Price (x $1,000)",
+     ylab = "Frequency",
+     xlim = c(0, 70),
+     main = "Prices of 93 Models of 1993 Cars")
+
+# same with probability instead of frequency
+hist(Cars93$Price,
+     xlab = "Price (x $1,000)",
+     ylab = "Probability",
+     xlim = c(0, 70),
+     main = "Prices of 93 Models of 1993 Cars",
+     probability = TRUE)
+
+# adding density line
+hist(Cars93$Price,
+     xlab = "Price (x $1,000)",
+     ylab = "Probability",
+     xlim = c(0, 70),
+     main = "Prices of 93 Models of 1993 Cars",
+     probability = TRUE)
+lines(density(Cars93$Price))
+
+# histogram with ggplot2
 # (bins of $5,000 between $0 and $65,000)
-ggplot(data = Cars93) + aes(Price) + geom_histogram(breaks = seq(0, 65, 5)) +
-  xlab("Price (x $1,000)") + ylab("Frequency")
+ggplot(data = Cars93) +
+  aes(Price) +
+  geom_histogram(breaks = seq(0, 65, 5)) +
+  xlab("Price (x $1,000)") +
+  ylab("Frequency")
 
 
 
 ## barchart
 
 # distribution of type
-ggplot(data = Cars93) + aes(Cars93$Type) + geom_bar() +
-  xlab("Type") + ylab("Frequency")
+
+ggplot(data = Cars93) +
+  aes(Cars93$Type) +
+  geom_bar() +
+  xlab("Type") +
+  ylab("Frequency")
 
 
 
